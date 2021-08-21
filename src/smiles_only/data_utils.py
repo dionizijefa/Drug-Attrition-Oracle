8a30ac233ddfbbb2177e0263d9c8372d26f77492
dionizije <dionizije.fa@hotmail.com>
2021-08-21 13:43:09 +0200

    Fix y values

--- src/smiles_only/data_utils.py
+++ src/smiles_only/data_utils.py
@@ -331,17 +331,18 @@ def smiles2graph(data, withdrawn_col):
 
     smiles = data['smiles']
     y = data[withdrawn_col]
+    print(y)
     mol = Chem.MolFromSmiles(smiles)
 
     # atoms
     atom_features_list = []
     for atom in mol.GetAtoms():
         atom_features_list.append(atom_to_feature_vector(atom))
-    x = np.array(atom_features_list, dtype = np.int64)
+    x = np.array(atom_features_list, dtype=np.int64)
 
     # bonds
     num_bond_features = 3  # bond type, bond stereo, is_conjugated
-    if len(mol.GetBonds()) > 0: # mol has bonds
+    if len(mol.GetBonds()) > 0:  # mol has bonds
         edges_list = []
         edge_features_list = []
         for bond in mol.GetBonds():
@@ -357,20 +358,20 @@ def smiles2graph(data, withdrawn_col):
             edge_features_list.append(edge_feature)
 
         # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
-        edge_index = np.array(edges_list, dtype = np.int64).T
+        edge_index = np.array(edges_list, dtype=np.int64).T
 
         # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
-        edge_attr = np.array(edge_features_list, dtype = np.int64)
+        edge_attr = np.array(edge_features_list, dtype=np.int64)
 
     else:   # mol has no bonds
-        edge_index = np.empty((2, 0), dtype = np.int64)
-        edge_attr = np.empty((0, num_bond_features), dtype = np.int64)
+        edge_index = np.empty((2, 0), dtype=np.int64)
+        edge_attr = np.empty((0, num_bond_features), dtype=np.int64)
 
     graph = dict()
     graph['edge_index'] = torch.Tensor(edge_index).long()
     graph['edge_attr'] = torch.Tensor(edge_attr)
     graph['node_feat'] = torch.Tensor(x).long()
     graph['num_nodes'] = torch.Tensor(len(x))
-    graph['y'] = torch.Tensor(y)
+    graph['y'] = torch.Tensor([y])
 
     return Data(x=graph['node_feat'], edge_index=graph['edge_index'], edge_attr=graph['edge_attr'], y=graph['y'])
