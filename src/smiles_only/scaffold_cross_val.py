556519056ce9fca67f4baf26e7320725e342f1f0
dionizije <dionizije.fa@hotmail.com>
2021-09-17 18:49:13 +0200

    Update scripts for production

--- src/smiles_only/scaffold_cross_val.py
+++ src/smiles_only/scaffold_cross_val.py
@@ -1,20 +1,83 @@
+from pathlib import Path
+
+import numpy as np
+from rdkit import RDConfig, Chem
+from rdkit.Chem import HybridizationType, ChemicalFeatures
 from sklearn.model_selection import StratifiedKFold, train_test_split
 import pandas as pd
 from torch.utils.data import WeightedRandomSampler
-from torch_geometric.data import DataLoader
+from torch_geometric.data import DataLoader, Data
 from data_utils import smiles2graph
-from torch import Tensor
+from torch import Tensor, cat
 
-def scaffold_cross_val(data, withdrawn_col, batch_size, seed):
-    scaffolds_df = pd.DataFrame(data['scaffolds'].value_counts())
-    unique_scaffolds = list(scaffolds_df.loc[scaffolds_df['scaffolds'] == 1].index)
-    data_unique_scaffolds = data.loc[data['scaffolds'].isin(unique_scaffolds)]
+fdef_name = Path(RDConfig.RDDataDir) / 'BaseFeatures.fdef'
+factory = ChemicalFeatures.BuildFeatureFactory(str(fdef_name))
 
+
+def cross_val(data, withdrawn_col, batch_size, seed):
+    """Don't split rest of the splits on scaffolds"""
     cv_splitter = StratifiedKFold(
         n_splits=5,
         shuffle=True,
         random_state=seed,
     )
+
+    loaders = []
+    for k, (train_index, test_index) in enumerate(
+            cv_splitter.split(data, data[withdrawn_col])
+    ):
+
+        test = data.iloc[test_index]
+        test_data_list = []
+        for index, row in test.iterrows():
+            test_data_list.append(smiles2graph(row, withdrawn_col))
+        test_loader = DataLoader(test_data_list, num_workers=0, batch_size=batch_size)
+
+        train_set = data.iloc[train_index]
+
+        train, val = train_test_split(
+            train_set,
+            test_size=0.15,
+            stratify=train_set[withdrawn_col],
+            shuffle=True,
+            random_state=seed)
+
+        train_data_list = []
+        for index, row in train.iterrows():
+            train_data_list.append(smiles2graph(row, withdrawn_col))
+
+        # balanced sampling of the minority class
+        withdrawn = train[withdrawn_col].value_counts()[1]
+        approved = train[withdrawn_col].value_counts()[0]
+        class_sample_count = [approved, withdrawn]
+        weights = 1 / Tensor(class_sample_count)
+        samples_weights = weights[train[withdrawn_col].values]
+        sampler = WeightedRandomSampler(samples_weights,
+                                        num_samples=len(samples_weights),
+                                        replacement=True)
+        train_loader = DataLoader(train_data_list, num_workers=0, batch_size=batch_size,
+                                  sampler=sampler)
+
+        val_data_list = []
+        for index, row in val.iterrows():
+            val_data_list.append(smiles2graph(row, withdrawn_col))
+        val_loader = DataLoader(val_data_list, num_workers=0, batch_size=batch_size)
+
+        loaders.append([train_loader, val_loader, test_loader])
+
+    return loaders
+
+
+def scaffold_cross_val(data, withdrawn_col, batch_size, seed):
+    """Validation split on scaffolds"""
+    cv_splitter = StratifiedKFold(
+        n_splits=5,
+        shuffle=True,
+        random_state=seed,
+    )
+    scaffolds_df = pd.DataFrame(data['scaffolds'].value_counts())
+    unique_scaffolds = list(scaffolds_df.loc[scaffolds_df['scaffolds'] == 1].index)
+    data_unique_scaffolds = data.loc[data['scaffolds'].isin(unique_scaffolds)]
 
     loaders = []
     for k, (train_index, test_index) in enumerate(
@@ -70,6 +133,293 @@ def create_loader(data, withdrawn_col, batch_size):
     data_loader = DataLoader(data_list, num_workers=0, batch_size=batch_size)
 
     return data_loader
+
+def one_hot_vector(val, lst):
+    """Converts a value to a one-hot vector based on options in lst"""
+    if val not in lst:
+        val = lst[-1]
+    return map(lambda x: x == val, lst)
+
+def smiles2graph(smiles, withdrawn_col):
+    """
+    Converts SMILES string to graph Data object
+    :input: SMILES string (str)
+    :return: graph object
+    """
+
+    #smiles = smiles
+    #y = withdrawn_col
+
+    y = smiles[withdrawn_col]
+    smiles = smiles['standardized_smiles']
+    mol = Chem.MolFromSmiles(smiles)
+
+    # atoms
+    donor = []
+    acceptor = []
+    features = []
+    names = []
+    donor_string = []
+
+    for atom in mol.GetAtoms():
+        atom_feature_names = []
+        atom_features = []
+        atom_features += one_hot_vector(
+            atom.GetAtomicNum(),
+            [5, 6, 7, 8, 9, 15, 16, 17, 35, 53, 999]
+        )
+
+        atom_feature_names.append(atom.GetSymbol())
+        atom_features += one_hot_vector(
+            atom.GetTotalNumHs(),
+            [0, 1, 2, 3, 4]
+        )
+        atom_feature_names.append(atom.GetTotalNumHs())
+        atom_features += one_hot_vector(
+            atom.GetHybridization(),
+            [HybridizationType.S, HybridizationType.SP, HybridizationType.SP2, HybridizationType.SP3,
+             HybridizationType.SP3D, HybridizationType.SP3D2, HybridizationType.UNSPECIFIED]
+        )
+        atom_feature_names.append(atom.GetHybridization().__str__())
+
+        atom_features.append(atom.IsInRing())
+        atom_features.append(atom.GetIsAromatic())
+
+        if atom.GetIsAromatic() == 1:
+            atom_feature_names.append('Aromatic')
+        else:
+            atom_feature_names.append('Non-aromatic')
+
+        if atom.IsInRing() == 1:
+            atom_feature_names.append('Is in ring')
+        else:
+            atom_feature_names.append('Not in ring')
+
+        donor.append(0)
+        acceptor.append(0)
+
+        donor_string.append('Not a donor or acceptor')
+
+        atom_features = np.array(atom_features, dtype=int)
+        atom_feature_names = np.array(atom_feature_names, dtype=object)
+        features.append(atom_features)
+        names.append(atom_feature_names)
+
+    feats = factory.GetFeaturesForMol(mol)
+    for j in range(0, len(feats)):
+        if feats[j].GetFamily() == 'Donor':
+            node_list = feats[j].GetAtomIds()
+            for k in node_list:
+                donor[k] = 0
+                donor_string[k] = 'Donor'
+        elif feats[j].GetFamily() == 'Acceptor':
+            node_list = feats[j].GetAtomIds()
+            for k in node_list:
+                acceptor[k] = 1
+                donor_string[k] = 'Acceptor'
+
+    features = np.array(features, dtype=int)
+    donor = np.array(donor, dtype=int)
+    donor = donor[..., np.newaxis]
+    acceptor = np.array(acceptor, dtype=int).transpose()
+    acceptor = acceptor[..., np.newaxis]
+    x = np.append(features, donor, axis=1)
+    x = np.append(x, acceptor, axis=1)
+
+
+    donor_string = np.array(donor_string, dtype=object)
+    donor_string = donor_string[..., np.newaxis]
+
+    names = np.array(names, dtype=object)
+    names = np.append(names, donor_string, axis=1)
+
+
+    # bonds
+    num_bond_features = 3  # bond type, bond stereo, is_conjugated
+    if len(mol.GetBonds()) > 0:  # mol has bonds
+        edges_list = []
+        for bond in mol.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+
+            # add edges in both directions
+            edges_list.append((i, j))
+            edges_list.append((j, i))
+
+        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
+        edge_index = np.array(edges_list, dtype=np.int64).T
+
+    else:   # mol has no bonds
+        edge_index = np.empty((2, 0), dtype=np.int64)
+
+    graph = dict()
+    graph['edge_index'] = Tensor(edge_index).long()
+    graph['node_feat'] = Tensor(x)
+    graph['y'] = Tensor([y])
+    graph['feature_names'] = names
+
+    return Data(x=graph['node_feat'], edge_index=graph['edge_index'], y=graph['y'], feature_names=names)
+
+def smiles2graph_descriptors(data, withdrawn_col, descriptors_from):
+    """
+    Converts SMILES string to graph Data object
+    :input: SMILES string (str)
+    :return: graph object
+    """
+
+    smiles = data['smiles']
+    y = data[withdrawn_col]
+    descriptors = data[descriptors_from:]
+    mol = Chem.MolFromSmiles(smiles)
+
+    # atoms
+    donor = []
+    acceptor = []
+    features = []
+    names = []
+    donor_string = []
+
+    for atom in mol.GetAtoms():
+        atom_feature_names = []
+        atom_features = []
+        atom_features += one_hot_vector(
+            atom.GetAtomicNum(),
+            [5, 6, 7, 8, 9, 15, 16, 17, 35, 53, 999]
+        )
+
+        atom_feature_names.append(atom.GetSymbol())
+        atom_features += one_hot_vector(
+            atom.GetTotalNumHs(),
+            [0, 1, 2, 3, 4]
+        )
+        atom_feature_names.append(atom.GetTotalNumHs())
+        atom_features += one_hot_vector(
+            atom.GetHybridization(),
+            [HybridizationType.S, HybridizationType.SP, HybridizationType.SP2, HybridizationType.SP3,
+             HybridizationType.SP3D, HybridizationType.SP3D2, HybridizationType.UNSPECIFIED]
+        )
+        atom_feature_names.append(atom.GetHybridization().__str__())
+
+        atom_features.append(atom.IsInRing())
+        atom_features.append(atom.GetIsAromatic())
+
+        if atom.GetIsAromatic() == 1:
+            atom_feature_names.append('Aromatic')
+        else:
+            atom_feature_names.append('Non-aromatic')
+
+        if atom.IsInRing() == 1:
+            atom_feature_names.append('Is in ring')
+        else:
+            atom_feature_names.append('Not in ring')
+
+        donor.append(0)
+        acceptor.append(0)
+
+        donor_string.append('Not a donor or acceptor')
+
+        atom_features = np.array(atom_features, dtype=int)
+        atom_feature_names = np.array(atom_feature_names, dtype=object)
+        features.append(atom_features)
+        names.append(atom_feature_names)
+
+    feats = factory.GetFeaturesForMol(mol)
+    for j in range(0, len(feats)):
+        if feats[j].GetFamily() == 'Donor':
+            node_list = feats[j].GetAtomIds()
+            for k in node_list:
+                donor[k] = 0
+                donor_string[k] = 'Donor'
+        elif feats[j].GetFamily() == 'Acceptor':
+            node_list = feats[j].GetAtomIds()
+            for k in node_list:
+                acceptor[k] = 1
+                donor_string[k] = 'Acceptor'
+
+    features = np.array(features, dtype=int)
+    donor = np.array(donor, dtype=int)
+    donor = donor[..., np.newaxis]
+    acceptor = np.array(acceptor, dtype=int).transpose()
+    acceptor = acceptor[..., np.newaxis]
+    x = np.append(features, donor, axis=1)
+    x = np.append(x, acceptor, axis=1)
+
+
+    donor_string = np.array(donor_string, dtype=object)
+    donor_string = donor_string[..., np.newaxis]
+
+    names = np.array(names, dtype=object)
+    names = np.append(names, donor_string, axis=1)
+
+
+    # bonds
+    num_bond_features = 3  # bond type, bond stereo, is_conjugated
+    if len(mol.GetBonds()) > 0:  # mol has bonds
+        edges_list = []
+        for bond in mol.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+
+            # add edges in both directions
+            edges_list.append((i, j))
+            edges_list.append((j, i))
+
+        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
+        edge_index = np.array(edges_list, dtype=np.int64).T
+
+    else:   # mol has no bonds
+        edge_index = np.empty((2, 0), dtype=np.int64)
+
+    graph = dict()
+    graph['edge_index'] = Tensor(edge_index).long()
+    graph['node_feat'] = Tensor(x)
+    graph['y'] = Tensor([y])
+    graph['feature_names'] = names
+    graph['descriptors'] = Tensor(descriptors)
+
+    return Data(x=graph['node_feat'], edge_index=graph['edge_index'], y=graph['y'], descriptors=graph['descriptors'],
+                feature_names=names)
+
+def calibrate(model, calib_loader):
+    calib_probabilities = []
+    targets = []
+    for i in calib_loader:
+        calib_probabilities.append(model.forward(i))
+        targets.append(i.y)
+    calib_probabilities = np.array(cat(calib_probabilities).detach().cpu().numpy().flatten())
+    calib_probabilities = 1 / (1 + np.exp(-calib_probabilities))
+    targets = np.array(cat(targets).detach().cpu().numpy().flatten())
+    calibration_df = pd.DataFrame({'probabilities': calib_probabilities, 'class': targets})
+    approved_probabilities = calibration_df.loc[calibration_df['class'] == 0]['probabilities'].values
+    approved_probabilities = 1 - approved_probabilities
+    withdrawn_probabilities = calibration_df.loc[calibration_df['class'] == 1]['probabilities'].values
+    approved_probabilities = np.sort(approved_probabilities)
+    withdrawn_probabilities = np.sort(withdrawn_probabilities)
+
+    return approved_probabilities, withdrawn_probabilities
+
+def conformal_prediction(test_loader, model, approved_probabilities, withdrawn_probabilities):
+    test_probabilities = []
+    test_targets = []
+    for i in test_loader:
+        test_probabilities.append(model.forward(i))
+        test_targets.append(i.y)
+    test_probabilities = np.array(cat(test_probabilities).detach().cpu().numpy().flatten())
+    test_probabilities = 1 / (1 + np.exp(-test_probabilities))
+
+    p_values_approved = []
+    p_values_withdrawn = []
+    for i in test_probabilities:
+        p_values_approved.append(
+            (np.searchsorted(approved_probabilities, i) / (len(approved_probabilities) + 1))
+        )
+        p_values_withdrawn.append(
+            (np.searchsorted(withdrawn_probabilities, i) / (len(withdrawn_probabilities) + 1))
+        )
+
+    return p_values_approved, p_values_withdrawn, test_probabilities
+
+
 
 
 
--- src/smiles_only/train_gcn.py
+++ src/smiles_only/train_gcn.py
@@ -9,10 +9,11 @@ from skopt.space import Categorical, Integer
 from skopt.utils import use_named_args
 import click
 from EGConv_lightning import Conf, EGConvNet
-from scaffold_cross_val import scaffold_cross_val, create_loader
-from torch import cat
+from scaffold_cross_val import cross_val, create_loader, calibrate, conformal_prediction
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
+
+from src.train_test_split import train_test_split
 
 root = Path(__file__).resolve().parents[2].absolute()
 
@@ -24,11 +25,27 @@ root = Path(__file__).resolve().parents[2].absolute()
 @click.option('-conformal', default=False)
 @click.option('-withdrawn_col', default='wd_consensus_1')
 @click.option('-batch_size', default=16)
+@click.option('-epochs', default=100)
 @click.option('-gpu', default=1)
+@click.option('-production', default=False)
 @click.option('-seed', default=0)
-def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size, gpu, seed):
+def main(
+        train_data,
+        test_data,
+        withdrawn_col,
+        bayes_opt,
+        conformal,
+        batch_size,
+        epochs,
+        gpu,
+        production,
+        seed
+):
     data = pd.read_csv(root / 'data/{}'.format(train_data))[['standardized_smiles', withdrawn_col, 'scaffolds']]
     data = data.sample(frac=1, random_state=seed)  # shuffle
+
+    test_data = pd.read_csv(root / test_data)[['standardized_smiles', withdrawn_col, 'scaffolds']]
+    test_loader = create_loader(test_data, withdrawn_col, batch_size)
 
     dim_1 = Categorical([128, 256, 512, 1024, 2048], name='hidden_channels')
     dim_2 = Integer(1, 8, name='num_layers')
@@ -40,7 +57,7 @@ def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size,
     def inverse_ap(hidden_channels, num_layers, num_heads, num_bases):
         fold_ap = []
         fold_auroc = []
-        conf=Conf(
+        conf = Conf(
             batch_size=batch_size,
             reduce_lr=True,
             hidden_channels=hidden_channels,
@@ -49,7 +66,7 @@ def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size,
             num_bases=num_bases,
         )
 
-        for fold in scaffold_cross_val(data, withdrawn_col, batch_size, seed):
+        for fold in cross_val(data, withdrawn_col, batch_size, seed):
             model = EGConvNet(
                 conf.to_hparams(),
                 reduce_lr=conf.reduce_lr,
@@ -76,7 +93,7 @@ def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size,
 
             print("Starting training")
             trainer = pl.Trainer(
-                max_epochs=conf.epochs,
+                max_epochs=epochs,
                 gpus=[gpu],  # [0]  # load from checkpoint instead of resume
                 weights_summary='top',
                 callbacks=[early_stop_callback, model_checkpoint],
@@ -120,18 +137,33 @@ def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size,
         print('Res space: {}'.format(res.x))
         print('Time elapsed in hrs: {}'.format(elapsed))
 
-        results_path = Path(root / 'bayes_opt')
-        if not results_path.exists():
-            results_path.mkdir(exist_ok=True, parents=True)
-            with open(results_path / "bayes_opt.txt", "w") as file:
-                file.write("Bayes opt - EGConv")
-                file.write("\n")
+        if production:
+            results_path = Path(root / 'production')
+            if not results_path.exists():
+                results_path.mkdir(exist_ok=True, parents=True)
+                with open(results_path / "bayes_opt.txt", "w") as file:
+                    file.write("Bayes opt - EGConv")
+                    file.write("\n")
 
-            with open(results_path / "bayes_opt.txt", "a") as file:
-                print('Maximum AP: {}'.format(res.fun), file=file)
-                print('Res space: {}'.format(res.space), file=file)
-                file.write("\n")
-                file.write("\n")
+                with open(results_path / "bayes_opt.txt", "a") as file:
+                    print('Maximum AP: {}'.format(res.fun), file=file)
+                    print('Res space: {}'.format(res.space), file=file)
+                    file.write("\n")
+                    file.write("\n")
+
+        else:
+            results_path = Path(root / 'bayes_opt')
+            if not results_path.exists():
+                results_path.mkdir(exist_ok=True, parents=True)
+                with open(results_path / "bayes_opt.txt", "w") as file:
+                    file.write("Bayes opt - EGConv")
+                    file.write("\n")
+
+                with open(results_path / "bayes_opt.txt", "a") as file:
+                    print('Maximum AP: {}'.format(res.fun), file=file)
+                    print('Res space: {}'.format(res.space), file=file)
+                    file.write("\n")
+                    file.write("\n")
 
     if conformal:
         if bayes_opt:
@@ -155,11 +187,14 @@ def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size,
                 num_bases=6,
             )
 
-        cross_conformal_p_approved = []
-        cross_conformal_p_withdrawn = []
-        class_probabilities = []
+        if production:
+            conf.save_dir = '{}/production/'.format(root)
 
-        for fold in scaffold_cross_val(data, withdrawn_col, batch_size, seed):
+        cross_approved_p = []
+        cross_withdrawn_p = []
+        cross_probabilities = []
+
+        for fold in cross_val(data, withdrawn_col, batch_size, seed):
             model = EGConvNet(
                 conf.to_hparams(),
                 reduce_lr=conf.reduce_lr,
@@ -167,7 +202,7 @@ def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size,
 
             logger = TensorBoardLogger(
                 conf.save_dir,
-                name='egconv_inference',
+                name='egconv_conformal',
                 version='{}'.format(str(int(time()))),
             )
 
@@ -191,7 +226,7 @@ def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size,
 
             print("Starting training")
             trainer = pl.Trainer(
-                max_epochs=100,
+                max_epochs=epochs,
                 gpus=[gpu],  # [0]  # load from checkpoint instead of resume
                 weights_summary='top',
                 callbacks=[early_stop_callback, model_checkpoint],
@@ -202,67 +237,91 @@ def main(train_data, test_data, withdrawn_col, bayes_opt, conformal, batch_size,
 
             train_loader, val_loader, calib_loader = fold
             trainer.fit(model, train_loader, val_loader)
+            #calibrate the model
+            approved_calibration, withdrawn_calibration = calibrate(model, calib_loader)
+            p_values_approved, p_values_withdrawn, test_probabilities = conformal_prediction(
+                test_loader,
+                model,
+                approved_calibration,
+                withdrawn_calibration
+            )
+            cross_approved_p.append(p_values_approved)
+            cross_withdrawn_p.append(p_values_withdrawn)
 
-            #calibration data
-            calib_probabilities = []
-            targets = []
-            for i in calib_loader:
-                calib_probabilities.append(model.forward(i))
-                targets.append(i.y)
-            calib_probabilities = np.array(cat(calib_probabilities).detach().cpu().numpy().flatten())
-            calib_probabilities = 1 / (1 + np.exp(-calib_probabilities))
-            targets = np.array(cat(targets).detach().cpu().numpy().flatten())
-            calibration_df = pd.DataFrame({'probabilities': calib_probabilities, 'class': targets})
-            approved_probabilities = calibration_df.loc[calibration_df['class'] == 0]['probabilities'].values
-            approved_probabilities = 1 - approved_probabilities
-            withdrawn_probabilities = calibration_df.loc[calibration_df['class'] == 1]['probabilities'].values
-            approved_probabilities = np.sort(approved_probabilities)
-            withdrawn_probabilities = np.sort(withdrawn_probabilities)
+        median_p_approved = np.median(np.array(cross_approved_p), axis=0)
+        median_p_withdrawn = np.median(np.array(cross_withdrawn_p), axis=0)
+        mean_p_approved = np.mean(np.array(cross_approved_p), axis=0)
+        mean_p_withdrawn = np.mean(np.array(cross_withdrawn_p), axis=0)
+        median_probabilities = np.median(np.array(cross_probabilities), axis=0)
+        mean_probabilities = np.median(np.array(cross_probabilities), axis=0)
 
-            test = pd.read_csv(root / 'data/{}'.format(test_data))[['standardized_smiles', withdrawn_col, 'scaffolds']]
-            test_loader = create_loader(test, withdrawn_col, conf.batch_size)
-            test_probabilities = []
-            test_targets = []
-            for i in test_loader:
-                test_probabilities.append(model.forward(i))
-                test_targets.append(i.y)
-            test_probabilities = np.array(cat(test_probabilities).detach().cpu().numpy().flatten())
-            test_probabilities = 1 / (1 + np.exp(-test_probabilities))
+        conformal_output = test_data[["chembl_id", withdrawn_col]]
+        conformal_output['median_p_approved'] = median_p_approved
+        conformal_output['median_p_withdrawn'] = median_p_withdrawn
+        conformal_output['mean_p_approved'] = mean_p_approved
+        conformal_output['mean_p_withdrawn'] = mean_p_withdrawn
+        conformal_output['median_probabilities'] = median_probabilities
+        conformal_output['mean_probabilities'] = mean_probabilities
 
-            p_values_approved = []
-            p_values_withdrawn = []
-            for i in test_probabilities:
-                p_values_approved.append(
-                    (np.searchsorted(approved_probabilities, i) / (len(approved_probabilities) + 1))
-                )
-                p_values_withdrawn.append(
-                    (np.searchsorted(withdrawn_probabilities, i) / (len(withdrawn_probabilities) + 1))
-                )
-
-            class_probabilities.append(test_probabilities)
-            cross_conformal_p_withdrawn.append(np.array(p_values_withdrawn))
-            cross_conformal_p_approved.append(np.array(p_values_approved))
-
-            #reset model
-            model = None
-
-
-        results_path = Path(root / 'cross_conformal_inference')
+        results_path = Path(root / 'cross_conformal')
         if not results_path.exists():
             results_path.mkdir(exist_ok=True, parents=True)
 
-        #merge targets and predicted values
-        targets = []
-        for i in test_loader:
-            targets.append(i.y)
-        targets = np.array(cat(targets).detach().cpu().numpy().flatten())
-        cross_conformal_p_approved = np.mean(np.array(cross_conformal_p_approved), axis=0)
-        cross_conformal_p_withdrawn = np.mean(np.array(cross_conformal_p_withdrawn), axis=0)
-        probabilities = np.mean(np.array(class_probabilities), axis=0)
-        test_df = pd.DataFrame({'probabilities': probabilities, 'class': targets,
-                                'p_value_approved': cross_conformal_p_approved,
-                                'p_value_withdrawn': cross_conformal_p_withdrawn})
-        test_df.to_csv(results_path / 'cross_conformal_predictions.csv')
+        conformal_output.to_csv(results_path / 'test_set_conformal.csv')
+
+    if production:
+        data = pd.concat([data, test_data])
+        approved_calibrations = []
+        withdrawn_calibrations = []
+        for count, fold in enumerate(cross_val(data, withdrawn_col, batch_size, seed)):
+            model = EGConvNet(
+                conf.to_hparams(),
+                reduce_lr=conf.reduce_lr,
+            )
+
+            logger = TensorBoardLogger(
+                conf.save_dir,
+                name='egconv_production',
+                version='{}'.format(count),
+            )
+
+            model_checkpoint = ModelCheckpoint(
+                dirpath=(logger.log_dir + '/checkpoint/'),
+                monitor='val_ap_epoch',
+                mode='max',
+                save_top_k=1,
+            )
+
+            early_stop_callback = EarlyStopping(monitor='val_ap_epoch',
+                                                min_delta=0.00,
+                                                mode='max',
+                                                patience=10,
+                                                verbose=False)
+
+            # Copy this script and all files used in training
+            log_dir = Path(logger.log_dir)
+            log_dir.mkdir(exist_ok=True, parents=True)
+            shutil.copy(Path(__file__), log_dir)
+
+            print("Starting training")
+            trainer = pl.Trainer(
+                max_epochs=epochs,
+                gpus=[gpu],  # [0]  # load from checkpoint instead of resume
+                weights_summary='top',
+                callbacks=[early_stop_callback, model_checkpoint],
+                deterministic=True,
+                auto_lr_find=False,
+                num_sanity_val_steps=0,
+            )
+
+            train_loader, val_loader, calib_loader = fold
+            trainer.fit(model, train_loader, val_loader)
+            approved_calibration, withdrawn_calibration = calibrate(model, calib_loader)
+            approved_calibrations.append(approved_calibration)
+            withdrawn_calibrations.append(withdrawn_calibration)
+
+        np.savetxt('approved_calibration.txt', np.array(approved_calibration), delimiter=',')
+        np.savetxt('withdrawn_calibration.txt', np.array(withdrawn_calibration), delimiter=',')
 
 
 if __name__ == '__main__':
