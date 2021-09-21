735ac131d4e09a50a4b73592f432490b1bc9b2ee
dionizijefa <dionizije.fa@hotmail.com>
2021-09-21 21:26:37 +0200

    imports working

--- src/smiles_only/EGConv_lightning.py
+++ src/smiles_only/EGConv_lightning.py
@@ -126,10 +126,7 @@ class EGConvNet(pl.LightningModule, ABC):
 
     def shared_step(self, data, batchidx):
         y_hat = self.model(data.x, data.edge_index, data.batch)
-        pos_weight = self.hparams.pos_weight.to("cuda")
-        loss_fn = torch.nn.BCEWithLogitsLoss(
-            #pos_weight=pos_weight
-        )
+        loss_fn = torch.nn.BCEWithLogitsLoss()
         loss = loss_fn(y_hat, data.y.unsqueeze(-1))
 
         return {
