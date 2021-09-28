6b7ba62c80e6e0ce9c3bf93e50f879428ef80a36
dionizije <dionizije.fa@hotmail.com>
2021-09-28 20:43:21 +0200

    Work on TDC inference for all models

--- src/TDC/tdc_inference.py
+++ src/TDC/tdc_inference.py
@@ -1,93 +1,90 @@
-from abc import ABC
 from pathlib import Path
-from typing import Optional
 import click
+import numpy as np
 import pandas as pd
-import pytorch_lightning as pl
 import torch
-from data_utils import load_data_from_smiles, construct_dataset
 from tqdm import tqdm
-from transformer import make_model
+from src.TDC import TDC_EGConv_lightning
+from src.utils.data_func import create_loader
+from src.smiles_only.EGConv_lightning import EGConvNet
 
 root = Path(__file__).resolve().parents[2].absolute()
 
-class TransformerNet(pl.LightningModule, ABC):
-    def __init__(
-            self,
-            reduce_lr: Optional[bool] = True,
-    ):
-        super().__init__()
-        self.reduce_lr = reduce_lr
-        self.model_params = {
-            'd_atom': 28,
-            'd_model': 1024,
-            'N': 8,
-            'h': 16,
-            'N_dense': 1,
-            'lambda_attention': 0.33,
-            'lambda_distance': 0.33,
-            'leaky_relu_slope': 0.1,
-            'dense_output_nonlinearity': 'relu',
-            'distance_matrix_kernel': 'exp',
-            'dropout': 0.0,
-            'aggregation_type': 'mean'
-        }
-        self.model = make_model(**self.model_params)
-
-    def forward(self, node_features, batch_mask, adjacency_matrix, distance_matrix):
-        out = self.model(node_features, batch_mask, adjacency_matrix, distance_matrix, None)
-        return out
-
-
 @click.command()
-@click.option('-train_data', default='chembl_4_smiles.csv')
-@click.option('-dataset', default='all')
-@click.option('-withdrawn_col', default='withdrawn')
-def main(train_data, dataset, withdrawn_col):
-    if dataset == 'all':
-        data = pd.read_csv(root / 'data/{}'.format(train_data))[['smiles', withdrawn_col, 'chembl_id']]
+@click.option('-train_data', default='processing_pipeline/train/train.csv')
+@click.option('-test_data', default='processing_pipeline/test/test.csv')
+@click.option('-withdrawn_col', default='wd_consensus_1')
+@click.option('-batch_size', default=32)
+@click.option('-seed', default=0)
+def main(train_data, test_data, withdrawn_col, batch_size, seed):
+    data = pd.read_csv(root / 'data/{}'.format(train_data))[['standardized_smiles', withdrawn_col,'chembl_id']][:10]
+    data = data.sample(frac=1, random_state=seed)  # shuffle
+    test_data = pd.read_csv(root / 'data/{}'.format(test_data))[['standardized_smiles', withdrawn_col, 'chembl_id']]
 
-    else:
-        data = pd.read_csv(root / 'data/{}'.format(train_data))
-        data = data.loc[(data['dataset'] == dataset) |
-                        (data['dataset'] == 'both') |
-                        (data['dataset'] == 'withdrawn')][['smiles', withdrawn_col, 'chembl_id']]
+    data_loader = create_loader(data, withdrawn_col, batch_size)
+    test_loader = create_loader(test_data, withdrawn_col, batch_size)
 
-    X_train, y_train = load_data_from_smiles(data['smiles'], data[withdrawn_col],
-                                             one_hot_formal_charge=True)
+    admes = (root / 'production/TDC/production')
 
-    admes = (root / 'models/TDC/transformer_net')
+    train_results = {}
+    test_results = {}
 
-    results = {}
     task_counter = 0
-    num_tasks = 37
+    num_tasks = 38
     for subdir in admes.iterdir():
         task_name = str(subdir).split('/')[-1]
         print('Doing inference for task {}/{}: {}'.format(task_counter, num_tasks, task_name))
         for file in Path(subdir / 'checkpoint').iterdir():
             checkpoint_file = file
 
-        outputs = []
-        model = TransformerNet.load_from_checkpoint(checkpoint_path=str(checkpoint_file), task=task_name)
+        model = TDC_EGConv_lightning.load_from_checkpoint(checkpoint_path=str(checkpoint_file), problem='inference')
         model.eval()
-        for i in tqdm(X_train):
-            node_features = torch.Tensor(i[0]).unsqueeze(0)
-            adjacency_matrix = torch.Tensor(i[1]).unsqueeze(0)
-            distance_matrix = torch.Tensor(i[1]).unsqueeze(0)
-            batch_mask = torch.sum(torch.abs(node_features), dim=-1) != 0
-            outputs.append(model.forward(node_features, batch_mask, adjacency_matrix, distance_matrix).squeeze(
-                -1).detach().cpu().numpy()[0])
 
-        results[task_name] = outputs
+        train_outputs = []
+        for i in tqdm(data_loader):
+            train_outputs.append(model.forward(i.x, i.edge_index, i.batch))
+        train_outputs = 1 / (1 + np.exp(-train_outputs))
+
+        test_outputs = []
+        for i in tqdm(test_loader):
+            test_outputs.append(model.forward(i.x, i.edge_index, i.batch))
+        test_outputs = 1 / (1 + np.exp(-test_outputs))
+
+        train_results[task_name] = train_outputs
+        test_results[task_name] = test_outputs
         print('Finished inference for task: {}'.format(task_name))
         task_counter += 1
         print('\n')
 
-    results_df = pd.DataFrame(results)
-    results_df['smiles'] = data['smiles'].values
-    results_df['chembl_id'] = data['chembl_id'].values
-    results_df['withdrawn'] = data[withdrawn_col].values
-    results_df.to_csv(root / 'data/subtasks_predictions.csv')
+    withdrawn_model_path = root / 'production/egconv_production/production/checkpoint/epoch=6-step=398.ckpt'
+    withdrawn_model = EGConvNet.load_from_checkpoint(checkpoint_path=withdrawn_model_path)
+    withdrawn_model.eval()
+
+    train_outputs = []
+    for i in tqdm(data_loader):
+        train_outputs.append(withdrawn_model.forward(i.x, i.edge_index, i.batch))
+    train_outputs = 1 / (1 + np.exp(-train_outputs))
+
+    test_outputs = []
+    for i in tqdm(test_loader):
+        test_outputs.append(withdrawn_model.forward(i.x, i.edge_index, i.batch))
+    test_outputs = 1 / (1 + np.exp(-test_outputs))
+
+    train_results['predict_withdrawn'] = train_outputs
+    test_results['predict_withdrawn'] = test_outputs
+
+
+    train_results_df = pd.DataFrame(train_results)
+    train_results_df['smiles'] = data['smiles'].values
+    train_results_df['chembl_id'] = data['chembl_id'].values
+    train_results_df['{}'.format(withdrawn_col)] = data[withdrawn_col].values
+    train_results_df.to_csv(root / 'data/TDC/train_subtasks_predictions.csv')
+
+    test_results_df = pd.DataFrame(train_results)
+    test_results_df['smiles'] = test_data['smiles'].values
+    test_results_df['chembl_id'] = test_data['chembl_id'].values
+    test_results_df['{}'.format(withdrawn_col)] = test_data[withdrawn_col].values
+    test_results_df.to_csv(root / 'data/TDC/train_subtasks_predictions.csv')
 
 
 if __name__ == '__main__':
