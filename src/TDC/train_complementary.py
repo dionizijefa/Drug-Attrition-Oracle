e75f87c3a0f0c3928c7af13c5a70022962177f7a
dionizije <dionizije.fa@hotmail.com>
2021-09-29 09:39:27 +0200

    Complementary RF training script

--- src/TDC/train_complementary.py
+++ src/TDC/train_complementary.py
@@ -3,7 +3,11 @@ import click
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
 from sklearn.model_selection import RandomizedSearchCV
+import cPickle
+
+from src.utils.metrics import table_metrics
 
 root = Path(__file__).resolve().parents[2].absolute()
 
@@ -28,7 +32,8 @@ def main(train_data, test_data, withdrawn_col, seed):
                    'max_features': max_features,
                    'max_depth': max_depth,
                    'min_samples_split': min_samples_split,
-                   'min_samples_leaf': min_samples_leaf}
+                   'min_samples_leaf': min_samples_leaf,
+                   'bootstrap': bootstrap}
 
     rf = RandomForestClassifier(class_weight='balanced')
     print("Running 100 iterations of random search of Random Forest optimization")
@@ -42,7 +47,39 @@ def main(train_data, test_data, withdrawn_col, seed):
 
     rf_random.fit(X_train, y_train)
     predictions = rf_random.best_estimator_.predict_proba(X_test)
-    print(predictions)
+
+    predictions_train = rf_random.best_estimator_.predict_proba(X_train)
+    train_pred_df = pd.DataFrame({'probabilities': predictions_train,
+                                  'target': y_train})
+
+    #optimal threshold F1 withdrawn class random forest
+    optimal_f1_score = []
+    optimal_threshold = []
+    for threshold in np.arange(0, 1, 0.01):
+        predictions_df = train_pred_df.copy()
+        predictions_df['predicted_class'] = 0
+        predictions_df.loc[predictions_df['probabilities'] > threshold, 'predicted_class'] = 1
+        optimal_f1_score.append(f1_score(
+            predictions_df['target'], predictions_df['predicted_class'], average='binary'
+        ))
+        optimal_threshold.append(threshold)
+
+    optimal_f1_index = np.argmax(np.array(optimal_f1_score))
+    optimal_threshold = optimal_threshold[optimal_f1_index]
+
+    test_pred_df = pd.DataFrame({'probabilities': predictions,
+                                withdrawn_col: y_test})
+
+    results_path = Path(root / 'complementary_model_results')
+    if not results_path.exists():
+        results_path.mkdir(exist_ok=True, parents=True)
+
+    results = table_metrics(test_pred_df, withdrawn_col, optimal_threshold)
+    results.to_csv(results_path / 'complementary_results.csv')
+
+    #If folder exists - add production complementary models
+    optimization_results = pd.DataFrame(rf_random.cv_results_)
+    optimization_results.to_csv(results_path / 'complementary_optimization.csv')
 
 if __name__ == '__main__':
     main()
